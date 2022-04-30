d613b1251d450bbf13ba51e67a163b1145f521d1
assraf-nur <assraf.nur@gmail.com>
2022-05-01 00:47:47 +0600

    added a delete api

--- index.js
+++ index.js
@@ -29,6 +29,20 @@ async function run(){
             const result = await productsCollection.findOne(query);
             res.send(result);
         });
+
+        app.post('/products', async(req, res)=>{
+            const newProducts = req.body;
+            const result = await productsCollection.insertOne(newProducts);
+            res.send(result);
+        })
+
+        app.delete('/products/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally{
 
