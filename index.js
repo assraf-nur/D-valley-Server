81c6cc4df059a589162184d98cd4db47613fe792
assraf-nur <assraf.nur@gmail.com>
2022-05-02 12:48:21 +0600

    add token

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT (req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'Unauthorized'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, decoded) =>{
+    if(err){
+      return res.status(403).send({message: 'Forbidden Access'})
+    }
+    req.decoded = decoded;
+  })
+  next();
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mydata.zdc33.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -82,7 +97,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/myProducts", async (req, res) => {
+    app.get("/myProducts", verifyJWT, async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const cursor = productsCollection.find(query);
