b1c52cc1fc33472b12bee74667d259ed22a95810
assraf-nur <assraf.nur@gmail.com>
2022-05-08 23:31:10 +0600

    db pass

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ async function run() {
       res.send(result);
     });
 
-    // delete product
+    // delete products
     app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
