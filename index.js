accf9676c26cf9c910b0025ed082bb31d2d7916e
assraf-nur <assraf.nur@gmail.com>
2022-05-08 13:50:18 +0600

    fix issue

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ async function run() {
       res.send(result);
     });
 
-    // update
+    // update product
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
