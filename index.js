a426a65ecf46373fff5dc49a9ef8c951edeb9eb5
assraf-nur <assraf.nur@gmail.com>
2022-05-08 13:45:14 +0600

    add user pass

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ async function run() {
     app.get("/login", async (req, res) => {
       const user = req.body;
       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "15d",
+        expiresIn: "25d",
       });
       res.send({ accessToken });
     });
